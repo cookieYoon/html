d2ee3a9c65b18cb07daf30a53f35c176cebc0a51
Cookie Yoon <my.cookie.yoon@gmail.com>
2022-01-20 00:46:24 +0900

    Create 21-2.php

---
+++ 21-2.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meta http-equiv = "Content-Type" content = "text/html;charset=utf-8">
+  </head>
+  <body>
+    <?php
+    echo 'name: '$_REQUEST['name'].' gender: '$_REQUEST['gender'].;
+    ?>
+  </body>
+</html>
